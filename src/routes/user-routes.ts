f18e26219ef8f35445022bf81dd83cdfa78445b2
LeonardoSantosBR <leosantosmelo07@gmail.com>
2024-05-28 12:24:10 -0300

    fix: outras rotas user

--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -1,11 +1,26 @@
 import { Router } from "express";
 import { userController } from "../usecases/users";
 
-
 const userRouter = Router();
 
-userRouter.post("/create/user", (request, response) => {
+userRouter.post("/user", (request, response) => {
   return userController.create(request, response);
+});
+
+userRouter.get("/user", (request, response) => {
+  return userController.findAll(request, response);
+});
+
+userRouter.get("/user/:id", (request, response) => {
+  return userController.findOne(request, response);
+});
+
+userRouter.patch("/user/:id", (request, response) => {
+  return userController.update(request, response);
+});
+
+userRouter.delete("/user/:id", (request, response) => {
+  return userController.delete(request, response);
 });
 
 export { userRouter };
